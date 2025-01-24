263f7ceaf1fc9fb4f035c48440215d40f2f7d240
jeffreyThe3155 <jjl28282@gmail.com>
2025-01-24 21:52:45 +0000

    commit

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,5 +2,19 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        for (int i = 0; i < input.length - 1; i++) {
+            //extra line to add stuff
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[i]) {
+                    int smaller = input[j];
+                    input[j] = input[i];
+                    input[i] = smaller;
+                }
+            }
+            //extra line to add stuff
+        }
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + " ");
+        }
     }
 }
