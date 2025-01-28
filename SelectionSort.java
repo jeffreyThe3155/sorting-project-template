721a4cf75355d069e54b00d48a5cdd44c6402c96
jeffreyThe3155 <jjl28282@gmail.com>
2025-01-28 00:11:57 +0000

    commit

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,4 +1,4 @@
-public class SelectionSort implements Sorter {
+/*public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
@@ -17,6 +17,27 @@ public class SelectionSort implements Sorter {
             input[i] = smallest;
         }
 
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + " ");
+        }
+    }
+}*/
+public class SelectionSort implements Sorter {
+
+    public void sort(int[] input) {
+        for (int i = 0; i < input.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[index]) {
+                    index = j;
+                }
+            }
+            if (index != i) {
+                int smallest = input[i];
+                input[i] = input[index];
+                input[index] = smallest;
+            }
+        }
         for (int i = 0; i < input.length; i++) {
             System.out.print(input[i] + " ");
         }
